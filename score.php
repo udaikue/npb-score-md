c459b9a3bbac40dbce0d46911554f25d23e74d2c
udaikue <ikueikue@gmail.com>
2022-10-15 11:47:00 +0900

    ファイルを追加

---
+++ score.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewpoint" content="width=device-width, initial-scale=1.0">
+  <title>試合結果をMarkdown形式で表示する</title>
+</head>
+<body>
+<?php
+
+?>
+</body>
+</html>
